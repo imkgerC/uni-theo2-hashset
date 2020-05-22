a242d9fb63c51fbab5e79960276d4796323d67fe
imkgerC <imrund08@gmail.com>
2020-05-22 12:55:00 +0200

    implement own variant of LinkedList to reduce size

--- src/hashset/chainingtable.rs
+++ src/hashset/chainingtable.rs
@@ -1,10 +1,36 @@
 use super::{HashTable, Hasher, ELEMENT_COUNT};
-use std::collections::LinkedList;
 use std::marker::PhantomData;
+
+enum LinkedList<T> {
+    Cons(T, Box<LinkedList<T>>),
+    Nil,
+}
+
+impl<T: PartialEq + Copy> LinkedList<T> {
+    pub fn contains(&self, searched: &T) -> bool {
+        match self {
+            LinkedList::Cons(val, other) => *val == *searched || other.contains(searched),
+            LinkedList::Nil => false,
+        }
+    }
+
+    pub fn push(&mut self, val: T) {
+        let push_to_self = match self {
+            LinkedList::Cons(_, other) => {
+                other.push(val);
+                false
+            }
+            LinkedList::Nil => true,
+        };
+        if push_to_self {
+            *self = LinkedList::Cons(val, Box::new(LinkedList::Nil));
+        }
+    }
+}
 
 pub struct DirectChainingTable<T: PartialEq + Copy, H: Hasher<T>> {
     collisions: usize,
-    entries: Vec<LinkedList<T>>,
+    entries: Vec<Box<LinkedList<T>>>,
     hasher: PhantomData<H>,
 }
 impl<T: PartialEq + Copy, H: Hasher<T>> Default for DirectChainingTable<T, H> {
@@ -17,7 +43,7 @@ impl<T: PartialEq + Copy, H: Hasher<T>> DirectChainingTable<T, H> {
     fn with_size(size: usize) -> Self {
         let mut entries = Vec::with_capacity(size);
         for _ in 0..size {
-            entries.push(LinkedList::new());
+            entries.push(Box::new(LinkedList::Nil));
         }
         Self {
             collisions: 0,
@@ -29,7 +55,7 @@ impl<T: PartialEq + Copy, H: Hasher<T>> DirectChainingTable<T, H> {
 
 pub struct SeparateChainingTable<T: PartialEq + Copy, H: Hasher<T>> {
     collisions: usize,
-    entries: Vec<(Option<T>, LinkedList<T>)>,
+    entries: Vec<(Option<T>, Box<LinkedList<T>>)>,
     hasher: PhantomData<H>,
 }
 impl<T: PartialEq + Copy, H: Hasher<T>> Default for SeparateChainingTable<T, H> {
@@ -42,7 +68,7 @@ impl<T: PartialEq + Copy, H: Hasher<T>> SeparateChainingTable<T, H> {
     fn with_size(size: usize) -> Self {
         let mut entries = Vec::with_capacity(size);
         for _ in 0..size {
-            entries.push((None, LinkedList::new()));
+            entries.push((None, Box::new(LinkedList::Nil)));
         }
         Self {
             collisions: 0,
@@ -61,11 +87,13 @@ impl<T: PartialEq + Copy, H: Hasher<T>> HashTable<T> for SeparateChainingTable<T
             }
             self.collisions += 1;
         }
-        for x in &self.entries[index].1 {
+        let mut right_now = self.entries[index].1.as_ref();
+        while let LinkedList::Cons(x, new) = right_now {
             if *x == *val {
                 return true;
             }
             self.collisions += 1;
+            right_now = new.as_ref();
         }
         false
     }
@@ -89,30 +117,32 @@ impl<T: PartialEq + Copy, H: Hasher<T>> HashTable<T> for SeparateChainingTable<T
         if self.entries[index].1.contains(val) {
             return true;
         }
-        self.entries[index].1.push_front(*val);
+        self.entries[index].1.push(*val);
         true
     }
     // the type is kind of complicated for this hash table so some assumptions will be made
-    // a bucket has size 40, any element that collides with another one takes up 24
+    // a bucket has size 16, any element that collides with another one takes up 16
     // if we assume that 0.25 of all elements have collided with another, our formula becomes
-    // 40*bytes + 0.25*24*elements
+    // 16*bytes + 0.25*16*elements
     fn resize_to_bytes(&mut self, bytes: usize, elements: usize) {
-        let available_bytes = bytes as isize - (elements as isize * 6);
+        let available_bytes = bytes as isize - (elements as isize * 4);
         if available_bytes < 1 {
             panic!("invalid configuration for direct chaining table");
         }
-        *self = Self::with_size(available_bytes as usize / 40);
+        *self = Self::with_size(available_bytes as usize / 16);
     }
 }
 
 impl<T: PartialEq + Copy, H: Hasher<T>> HashTable<T> for DirectChainingTable<T, H> {
     fn has(&mut self, val: &T) -> bool {
         let index = H::hash(val, self.entries.len());
-        for x in &self.entries[index] {
+        let mut right_now = self.entries[index].as_ref();
+        while let LinkedList::Cons(x, next) = right_now {
             if *x == *val {
                 return true;
             }
             self.collisions += 1;
+            right_now = next.as_ref();
         }
         false
     }
@@ -125,19 +155,19 @@ impl<T: PartialEq + Copy, H: Hasher<T>> HashTable<T> for DirectChainingTable<T, 
     fn insert(&mut self, val: &T) -> bool {
         let index = H::hash(val, self.entries.len());
         if !self.entries[index].contains(val) {
-            self.entries[index].push_front(*val);
+            self.entries[index].push(*val);
         }
         true
     }
 
     // size of direct chaining table is buckets*(size of bucket) + entries*(size of node)
-    // size of bucket is the size of an empty linkedlist = 24
-    // size of node is the size of a linkedlist node = 24
+    // size of bucket is the size of a pointer to a linkedlist = 8
+    // size of node is the size of a linkedlist node = 16
     fn resize_to_bytes(&mut self, bytes: usize, elements: usize) {
-        let available_bytes = bytes as isize - (24 * elements) as isize;
+        let available_bytes = bytes as isize - (16 * elements) as isize;
         if available_bytes < 1 {
             panic!("not enough bytes available for the buckets");
         }
-        *self = Self::with_size(available_bytes as usize / 24);
+        *self = Self::with_size(available_bytes as usize / 8);
     }
 }
